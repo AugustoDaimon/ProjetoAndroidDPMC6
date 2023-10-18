373674c714019d8adf3c05a3d285cb151dabfba2
augusto <augustosasaki@hotmail.com>
2023-10-18 09:33:36 -0300

    Subpaginas de cadastro

--- app/src/main/java/com/example/nutzen/MainActivity.java
+++ app/src/main/java/com/example/nutzen/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.nutzen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /*Button botao = (Button) findViewById(R.id.botao);
+
+    botao.setBackgroundResource(R.color.Red);
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/nutzen/MainActivity5.java
@@ -0,0 +1,16 @@
+package com.example.nutzen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+    /*variaveis*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+}
