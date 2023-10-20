cc61566146e890e49af6a76fbad449e49da1b3ff
AugustoDaimon <augustosasaki@hotmail.com>
2023-10-20 00:43:08 -0300

    Topo da Tela Cadastro SeekBar customizada V1.0

--- app/src/main/java/com/example/nutzen/MainActivity.java
+++ app/src/main/java/com/example/nutzen/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.nutzen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastrar = findViewById(R.id.btn_cadastrar);
+        btnCadastrar.setOnClickListener(view -> { // <- Erro Aqui
+
+            Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+            startActivity(intent);
+        });
     }
 }
