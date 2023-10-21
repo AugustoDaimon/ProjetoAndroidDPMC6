596f2d46002139342add40b919149a962720324d
AugustoDaimon <augustosasaki@hotmail.com>
2023-10-21 05:31:18 -0300

    Adicao de FragmentView em Activity Cadastro+ Manipulacao de Fragmentos V1.0

--- app/src/main/java/com/example/nutzen/MainActivity.java
+++ app/src/main/java/com/example/nutzen/MainActivity.java
@@ -6,17 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.nutzen.Cadastro.CadastroActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCadastrar;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCadastrar = findViewById(R.id.btn_cadastrar);
-        btnCadastrar.setOnClickListener(view -> { // <- Erro Aqui
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(view -> {
 
             Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
             startActivity(intent);
