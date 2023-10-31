fc96f079479ecce4bfb83a1a7b4d7e0125ebd8b3
AugustoDaimon <augustosasaki@hotmail.com>
2023-10-30 22:03:12 -0300

    Lucas: Telas 7 a 10

---
+++ app/src/main/java/com/example/nutzen/MainActivity8.java
@@ -0,0 +1,14 @@
+package com.example.nutzen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main8);
+    }
+}
